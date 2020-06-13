4ff7211094a15d2e9cade8b9471d799c12eddabd
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-06-13 13:46:44 +0530

    Form

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,112 +11,78 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      username: '',
-      password: '',
+      store_name: '',
+      store_size: '',
+      location:'',
+      employee_count:'',
+      max_customers:'',
     };
   }
 
-  onLogin() {
-    const { username, password } = this.state;
+  onRegister() {
+    const { store_name, max_customers } = this.state;
 
-    Alert.alert('Credentials', `${username} + ${password}`);
+    Alert.alert(`${store_name}`, `${max_customers} Customer limit set`);
   }
 
   render() {
     return (
       <View style={styles.container}>
         <TextInput
-          value={this.state.username}
-          onChangeText={(username) => this.setState({ username })}
-          placeholder={'Username'}
+          value={this.state.store_name}
+          onChangeText={(store_name) => this.setState({ store_name })}
+          placeholder={'Store Name'}
           style={styles.input}
         />
         <TextInput
-          value={this.state.password}
-          onChangeText={(password) => this.setState({ password })}
-          placeholder={'Password'}
-          secureTextEntry={true}
+          value={this.state.store_size}
+          onChangeText={(store_size) => this.setState({ store_size })}
+          placeholder={'Store Size'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.location}
+          onChangeText={(location) => this.setState({ location })}
+          placeholder={'Location'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.employee_count}
+          onChangeText={(employee_count) => this.setState({ employee_count })}
+          placeholder={'Employee Count'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.max_customers}
+          onChangeText={(max_customers) => this.setState({ max_customers })}
+          placeholder={'Max Customers'}
           style={styles.input}
         />
 
         <Button
-          title={'Login'}
+          title={'Register'}
           style={styles.input}
-          onPress={this.onLogin.bind(this)}
+          onPress={this.onRegister.bind(this)}
         />
       </View>
     );
   }
 }
 
-
-function LinksScreen() {
-
-  return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-      <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-      />
-
-      <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
-      />
-
-      <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-        isLastOption
-      />
-    </ScrollView>
-  );
-}
-
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+    color: 'black'
   },
 });
